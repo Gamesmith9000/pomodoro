0321592bd57c0437db462fe0d5fdc5c70a964700
Julian Smith <smith.julian.f@gmail.com>
2021-10-04 11:41:10 -0500

    Made timer robust, using set/clearInterval in a more properly contained way

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,37 +1,59 @@
-import React, { FC, useState } from 'react';
+import React, { FC, MutableRefObject, useRef, useState } from 'react';
 
 export const Timer:FC<TimerProps> = (props) => {
+    const intervalId: MutableRefObject<number> = useRef(0);
     const [currentTime, setCurrentTime] = useState(0);
     const [hasCompleted, setHasCompleted] = useState(false);
-    // const [intervalId, setintervalId] = useState(0);
 
     const remainingSeconds: number = props.duration - currentTime;
     const remainingFullMinutes: number = Math.floor(remainingSeconds / 60);
     const remainingSecondsLessMinutes: number = remainingSeconds - (remainingFullMinutes * 60);
-    
-    if(hasCompleted === false){
+
+    const removeIntervalData = () => {
+        window.clearInterval(intervalId.current);
+        intervalId.current = 0;
+    }
+
+    if(hasCompleted === false) {
         if(currentTime >= props.duration) {
             props.onCompleteCountdown();
+            removeIntervalData();
             setHasCompleted(true);
         }
         else {
-            setTimeout(() => { setCurrentTime(currentTime + 1); }, 1000);
+            if(props.isRunning === true && intervalId.current === 0) {
+                intervalId.current = window.setInterval(() => { setCurrentTime(current => current + 1) }, 1000);
+            }
+            else if(props.isRunning === false && intervalId.current !== 0) {
+                removeIntervalData();
+            }
         }
     }
 
     return (
         <div>
-            <p>remainingSeconds:{remainingSeconds}</p>
-            <p>remainingFullMinutes:{remainingFullMinutes}</p>
-            <p>remainingSecondsLessMinutes:{remainingSecondsLessMinutes}</p>
+            {addLeadingZero(remainingFullMinutes)}
+            {"\u00a0"}:{"\u00a0"}
+            {addLeadingZero(remainingSecondsLessMinutes)} 
         </div>
     );
+}
+
+const addLeadingZero = (num: number) => {
+    const numAsString = String(num);
+    return num < 10 ? '0' + numAsString : numAsString;
+}
+
+interface RemaingTimes {
+    fullMinutes: number;
+    seconds: number;
+    secondsLessMinutes: number;
 }
 
 interface TimerProps {
     duration: number;
     initiallyInSittingPosition: boolean;
+    isRunning: boolean;
     onCompleteCountdown(): any,
     positionMatchesInitial: boolean;
 }
-
